1d0a260ff85c3e4317ad814ecb6f4afe88454d14
Roberto Cunha <robertocunha@gmail.com>
2023-09-05 00:52:31 -0300

    commit final

--- script4.js
+++ script4.js
@@ -6,9 +6,26 @@ const arrayImagens = [
 ];
 
 let imagem = document.querySelector("img");
+let indice = 0;
+let nroImagens = arrayImagens.length;
+console.log(nroImagens)
 
-imagem.src = "/imagens/imagem2.jfif";
+imagem.src = arrayImagens[indice];
 
 function avancar() {
-    
+    if (indice === nroImagens - 1) {
+        indice = 0;
+    } else {
+        indice += 1;
+    }
+    imagem.src = arrayImagens[indice];
+}
+
+function voltar() {
+    if (indice === 0) {
+        indice = nroImagens - 1;
+    } else {
+        indice -= 1;
+    }
+    imagem.src = arrayImagens[indice];
 }
